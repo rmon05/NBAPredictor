c9eb0a84d76c8952b891c5171afcdd9dd5b42754
rmon05 <rayhappyfun@gmail.com>
2025-12-07 19:11:05 -0500

    Fixes for tailwind 4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,14 @@ import path from "path"
 import { fileURLToPath } from "url"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tailwindcss from '@tailwindcss/vite';
 
 // This "fix" enables __dirname on all systems (ESM modules)
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
